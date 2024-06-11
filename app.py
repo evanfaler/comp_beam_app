932e0f666396f59698977c2c75e2e1bf691afdbd
evanfaler <emfaler@gmail.com>
2024-06-11 12:34:48 -0400

    Added default values for testing

--- app.py
+++ app.py
@@ -15,46 +15,37 @@ with st.sidebar.expander(label='Span Definition', expanded=True):
 with st.sidebar.expander(label='Beam Layout', expanded=True):
     col1, col2 = st.columns(2)
     with col1:
-        trib_left = st.number_input(label='Distance left (ft)', key='left_dist')
+        trib_left = st.number_input(label='Distance left (ft)', key='left_dist', value=8.0)
         left_cond = st.selectbox('Dist to Edge or Beam?', ['Beam', 'Edge'], key='left_cond')
         
     with col2:
-        trib_right = st.number_input(label='Distance right (ft)', key='right_dist')
+        trib_right = st.number_input(label='Distance right (ft)', key='right_dist', value=8.0)
         right_cond = st.selectbox('Dist to Edge or Beam?', ['Beam', 'Edge'], key='right_cond')
 
 with st.sidebar.expander(label='Stud Information', expanded=True):
     stud_fu = st.number_input(label='Fu (ksi)', value=65, step=5, key='stud_fu')
     stud_dia = st.selectbox(label='Diameter', options=['3/4"', '5/8"', '1/2"'], key='stud_dia')
-    stud_length = st.number_input(label='Length (in)', value=4.0, step=0.5, key='stud_length')
+    stud_length = st.number_input(label='Length (in)', value=5.0, step=0.5, key='stud_length')
 
 # Deck and Fill
 with st.sidebar.expander(label='Deck and Fill', expanded=True):
-    conc_thickness = st.number_input(label='Conc thickness above flutes (in)', value=4.0, step=0.5, key='conc_thickness')
+    conc_thickness = st.number_input(label='Conc thickness above flutes (in)', value=3.5, step=0.5, key='conc_thickness')
     fc = st.number_input(label='Conc strength (ksi)', value=4.0, step=0.5, min_value=3.0, max_value=10.0, key='fc')
     lightweight = st.checkbox('Lightweight', key='lightweight')
-    deck_dir = st.selectbox(label='Deck Direction (deg)', options=[0,90], key='deck_dir')
-    deck_height = st.number_input(label='Height of deck (in)', value=1.5, step=0.5, key='deck_height')
+    deck_dir = st.selectbox(label='Deck Direction (deg)', options=[0,90], key='deck_dir', index=1)
+    deck_height = st.number_input(label='Height of deck (in)', value=3.0, step=0.5, key='deck_height')
 
 # Uniform Loading
 with st.sidebar.expander(label='Uniform Loads', expanded=True):
     format_str = '%.3f'
-    uniform_dead = st.number_input(label='Dead Load (kip/ft)', format=format_str, key='uniform_dead')
-    uniform_const_dead = st.number_input(label='Construction Dead Load (kip/ft)', format=format_str, key='uniform_const_dead')
-    uniform_live = st.number_input(label='Live Load (kip/ft)', format=format_str, key='uniform_live')
-    uniform_partition_live = st.number_input(label='Partion Live Load (kip/ft)', format=format_str, key='uniform_partition_live')
-    uniform_const_live = st.number_input(label='Construction Live Load (kip/ft)', format=format_str, key='uniform_const_live')
+    uniform_dead = st.number_input(label='Dead Load (kip/ft)', format=format_str, key='uniform_dead', value=0.584)
+    uniform_const_dead = st.number_input(label='Construction Dead Load (kip/ft)', format=format_str, key='uniform_const_dead', value=0.504)
+    uniform_live = st.number_input(label='Live Load (kip/ft)', format=format_str, key='uniform_live', value=0.800)
+    uniform_partition_live = st.number_input(label='Partion Live Load (kip/ft)', format=format_str, key='uniform_partition_live', value=0.000)
+    uniform_const_live = st.number_input(label='Construction Live Load (kip/ft)', format=format_str, key='uniform_const_live', value=0.200)
 
 st.write(st.session_state)
 
 calc_data = app_logic.calc_beam(st.session_state)
 
 st.write(calc_data)
-
-
-
-
-
-
-        
-
-
